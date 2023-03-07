34e6d1d0683d36b6ad239e3cfa64a5369d18050e
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-03-07 19:55:37 +0300

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -8,6 +8,4 @@ module full_adder(
 assign S= (A^B)^Cin;
 assign Cout=(A&B)|(B&Cin)|(Cin&A);
 
-    // Your code goes here.  DO NOT change anything that is already given! Otherwise, you will not be able to pass the tests!
-
 endmodule
