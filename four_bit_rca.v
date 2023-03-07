de2993e096250a16349d0cf34eb4c8b423c8ba60
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-03-07 19:55:22 +0300

    Update four_bit_rca.v

--- four_bit_rca.v
+++ four_bit_rca.v
@@ -11,6 +11,5 @@ full_adder A1(.A(A[0]),.B(B[0]),.Cin(Cin),.S(S[0]),.Cout(car[0]));
 full_adder A2(.A(A[1]),.B(B[1]),.Cin(car[0]),.S(S[1]),.Cout(car[1]));
 full_adder A3(.A(A[2]),.B(B[2]),.Cin(car[1]),.S(S[2]),.Cout(car[2]));
 full_adder A4(.A(A[3]),.B(B[3]),.Cin(car[2]),.S(S[3]),.Cout(Cout));
-    // Your code goes here.  DO NOT change anything that is already given! Otherwise, you will not be able to pass the tests!
 
 endmodule
