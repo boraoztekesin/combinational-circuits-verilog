13e335de1a2dd84dbcfa037fbdd4023d7224e7c5
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-03-07 19:54:41 +0300

    Update four_bit_adder_subtractor.v

--- four_bit_adder_subtractor.v
+++ four_bit_adder_subtractor.v
@@ -6,7 +6,6 @@ module four_bit_adder_subtractor(A, B, subtract, Result, Cout);
     output [3:0] Result;
     output Cout;
 
-    // Your code goes here.  DO NOT change anything that is already given! Otherwise, you will not be able to pass the tests!
   wire[3:0] Out1;
   wire[3:0] Out2;
   reg Cin;
