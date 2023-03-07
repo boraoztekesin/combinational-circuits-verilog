8b2b0c4a8280ad74d749652b09c4240929c5210c
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-03-07 19:55:55 +0300

    Update two_s_complement.v

--- two_s_complement.v
+++ two_s_complement.v
@@ -2,7 +2,6 @@ module two_s_complement(In,Out);
     input [3:0] In;
     output [3:0] Out;
     
-    // Your code goes here.  DO NOT change anything that is already given! Otherwise, you will not be able to pass the tests!
     assign Out= ~In + 1'b1;
 
 endmodule  
