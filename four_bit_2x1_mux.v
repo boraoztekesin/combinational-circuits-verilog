1d5b18e09ce783fc8ab089c1e35740239a9402e0
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-03-07 19:54:04 +0300

    Update four_bit_2x1_mux.v

--- four_bit_2x1_mux.v
+++ four_bit_2x1_mux.v
@@ -4,6 +4,5 @@ module four_bit_2x1_mux(In_1, In_0, Select, Out);
 	input Select;
 	output [3:0] Out;
 	
-	// Your code goes here.  DO NOT change anything that is already given! Otherwise, you will not be able to pass the tests!
 	assign Out = (Select) ? In_1 : In_0;
 endmodule
